07183bf917a2977619211720a862a999e0d82c0c
Bruno Assis <brunoassis@gmail.com>
2017-04-01 12:00:05 -0300

    Adds more tests to CreateAction

--- spec/controllers/properties/create_action_spec.cr
+++ spec/controllers/properties/create_action_spec.cr
@@ -44,12 +44,36 @@ module Spotippos::Controllers::Properties
         expect(response.status_code).to eq(201)
       end
 
-      it "returns the new property as a JSON" do
-        expect(JSON.parse(response.body)).to eq(JSON.parse(new_property_json))
+      it "returns the new property as a JSON with an id" do
+        response_fields = JSON.parse(response.body)
+        new_property_fields = JSON.parse(new_property_json)
+
+        expect(response_fields["id"].as_i64).to_be > 0_i64
+        expect(response_fields["lat"]).to eq(new_property_fields["lat"])
+        expect(response_fields["long"]).to eq(new_property_fields["long"])
+        expect(response_fields["title"]).to eq(new_property_fields["title"])
+        expect(response_fields["description"]).to eq(new_property_fields["description"])
+        expect(response_fields["beds"]).to eq(new_property_fields["beds"])
+        expect(response_fields["baths"]).to eq(new_property_fields["baths"])
+        expect(response_fields["provinces"]).to eq(new_property_fields["provinces"])
+        expect(response_fields["squareMeters"]).to eq(new_property_fields["squareMeters"])
       end
     end
 
     context "when the payload is invalid" do
+      context "because it is missing" do
+        let(payload) { nil }
+
+        it "returns 400 - Bad Request" do
+          expect(response.status_code).to eq(400)
+        end
+
+        it "returns an error message" do
+          error_json = {"error" => "Bad Request: Missing payload."}.to_json
+          expect(response.body).to eq(error_json)
+        end
+      end
+
       context "because of missing required values" do
         let(payload) do
           <<-JSON
@@ -68,12 +92,91 @@ module Spotippos::Controllers::Properties
         end
 
         it "returns an error message" do
-          error_json = {"error" => "Bad Request: Missing values: {lat}, {lng}, {description}"}.to_json
+          error_message = "Bad Request: {description} missing, {lat} missing, {long} missing"
+          error_json = {"error" => error_message}.to_json
           expect(response.body).to eq(error_json)
         end
       end
 
       context "because of validation errors" do
+        context "and its title is invalid" do
+          let(payload) do
+            <<-JSON
+            {
+              "lat": 333,
+              "long": 444,
+              "title": "",
+              "price": 1250000,
+              "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+              "beds": 4,
+              "baths": 3,
+              "squareMeters": 210
+            }
+            JSON
+          end
+
+          it "returns 400 - Bad Request" do
+            expect(response.status_code).to eq(400)
+          end
+
+          it "returns an error message" do
+            error_json = {"error" => "Bad Request: {title} cannot be blank"}.to_json
+            expect(response.body).to eq(error_json)
+          end
+        end
+
+        context "and its description is invalid" do
+          let(payload) do
+            <<-JSON
+            {
+              "lat": 333,
+              "long": 444,
+              "title": "Imóvel código 1, com 5 quartos e 4 banheiros",
+              "price": 1250000,
+              "description": "",
+              "beds": 4,
+              "baths": 3,
+              "squareMeters": 210
+            }
+            JSON
+          end
+
+          it "returns 400 - Bad Request" do
+            expect(response.status_code).to eq(400)
+          end
+
+          it "returns an error message" do
+            error_json = {"error" => "Bad Request: {description} cannot be blank"}.to_json
+            expect(response.body).to eq(error_json)
+          end
+        end
+
+        context "and its price is invalid" do
+          let(payload) do
+            <<-JSON
+            {
+              "lat": 333,
+              "long": 444,
+              "title": "Imóvel código 1, com 5 quartos e 4 banheiros",
+              "price": 0,
+              "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+              "beds": 4,
+              "baths": 3,
+              "squareMeters": 210
+            }
+            JSON
+          end
+
+          it "returns 400 - Bad Request" do
+            expect(response.status_code).to eq(400)
+          end
+
+          it "returns an error message" do
+            error_json = {"error" => "Bad Request: {price} must be greater than 0"}.to_json
+            expect(response.body).to eq(error_json)
+          end
+        end
+
         context "and its latitude is invalid" do
           let(payload) do
             <<-JSON
